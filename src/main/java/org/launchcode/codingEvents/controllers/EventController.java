6899beb6f917b164c4e59ac5e564447f6d1462c1
Ashley Hainline <aetindle@gmail.com>
2023-10-10 16:16:47 -0500

    add code for ch 13 exercises

--- src/main/java/org/launchcode/codingEvents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingEvents/controllers/EventController.java
@@ -4,17 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+
 
 @Controller
 @RequestMapping("/events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        events.put("St. Louis Code and Coffee", "Twice monthly coding meetup for all skill levels at TechArtista");
+        events.put("Code Til Dawn", "Once a month event for all tech specialties of all skill levels");
+        events.put("Women Who Code", "Group focused on inclusion in the tech industry putting on various events");
         model.addAttribute("events", events);
         return "events/index";
 
@@ -25,9 +28,9 @@ public class EventController {
         return "events/create";
     }
 
-    @PostMapping("/create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
-        return "redirect:/events";
-    }
+//    @PostMapping("/create")
+//    public String createEvent(@RequestParam String eventName) {
+//        events.add(eventName);
+//        return "redirect:/events";
+//    }
 }

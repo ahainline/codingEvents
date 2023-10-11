b628b37f9db3a1f8719f808c1dc64e209a0e61eb
Ashley Hainline <aetindle@gmail.com>
2023-10-11 14:33:39 -0500

    Add Ch 11 code for Models & Model Binding

--- src/main/java/org/launchcode/codingEvents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingEvents/controllers/EventController.java
@@ -1,33 +1,51 @@
 package org.launchcode.codingEvents.controllers;
 
+import org.launchcode.codingEvents.data.EventData;
+import org.launchcode.codingEvents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
-
     }
 
     @GetMapping("/create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     @PostMapping("/create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
         return "redirect:/events";
     }
+
+    @GetMapping("/delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("/delete")
+    public String processDeleteEventsForm(@RequestParam(required=false) int[] eventIds) {
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+
+        return "redirect:/events";
+    }
+
 }

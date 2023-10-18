f66b61fd770fa9b4842f043f744068f62d7b2360
Ashley Hainline <aetindle@gmail.com>
2023-10-18 15:24:19 -0500

    Add code for model validation - Ch15

--- src/main/java/org/launchcode/codingEvents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingEvents/controllers/EventController.java
@@ -1,9 +1,11 @@
 package org.launchcode.codingEvents.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.codingEvents.data.EventData;
 import org.launchcode.codingEvents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -21,11 +23,17 @@ public class EventController {
     @GetMapping("/create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
     @PostMapping("/create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model) {
+
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:/events";
     }
